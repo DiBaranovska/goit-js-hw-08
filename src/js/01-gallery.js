d0799a9a3dc30fe43e4e8c79f0e80e4a32bd4d28
Diana Baranovska <dmitrudi@gmail.com>
2023-03-10 22:06:12 +0200

    виконує перше завдання

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+const cards = creatGalleryCards(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', cards);
+
+function creatGalleryCards(image) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(galleryItems);
