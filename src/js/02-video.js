b0c6f060c3b16cd72168d8a028ee38433dcadef2
Diana Baranovska <dmitrudi@gmail.com>
2023-03-11 17:13:01 +0200

    Вирішує другу задачу

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import trottle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = function getCurrentTime(event) {
+  const time = event.seconds;
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+player.on('timeupdate', trottle(currentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
